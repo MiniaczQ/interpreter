6ec2b8a26228a3a055de56d6f0cc188211bca928
MiniaczQ <xnetroidpl@gmail.com>
2022-05-15 16:01:27 +0200

    code block refactor

--- src/parser/grammar/code_block.rs
+++ src/parser/grammar/code_block.rs
@@ -1,50 +1,39 @@
-use crate::{
-    parser::{
-        operators::Operator, token::TokenType, ErrorHandler, ExtScannable, Parser, ParserError,
-        ParserWarningVariant,
-    },
-    scannable::Scannable,
-};
+use super::utility::*;
 
-use super::{
-    expressions::{parse_expression, Expression},
-    ParseResult,
-};
+use super::expressions::{parse_expression, Expression};
 
-/// Block of code that returns the last espression
+/// Block of code that returns the last espression.
 #[derive(Debug, Clone)]
 pub struct CodeBlock {
     statements: Vec<Statement>,
 }
 
-/// code_block
-///     = OPEN_CODEBLOCK, statements, CLOSE_CODEBLOCK
-///     ;
-pub fn parse_code_block(p: &mut Parser) -> ParseResult<CodeBlock> {
-    if let TokenType::Operator(Operator::OpenCurlyBracket) = p.token()?.token_type {
-        p.pop();
-        let statements = parse_statements(p)?;
-        if let TokenType::Operator(Operator::CloseCurlyBracket) = p.token()?.token_type {
-            p.pop();
-        } else {
-            p.warn(ParserWarningVariant::MissingClosingCurlyBracket);
-        }
-        Ok(Some(CodeBlock { statements }))
-    } else {
-        Ok(None)
-    }
-}
-
+/// A single statement.
+/// Either an expression or a `;`.
 #[derive(Debug, Clone)]
 pub enum Statement {
     Expression(Expression),
     Semicolon,
 }
 
+/// code_block
+///     = OPEN_CODEBLOCK, statements, CLOSE_CODEBLOCK
+///     ;
+pub fn parse_code_block(p: &mut Parser) -> OptRes<CodeBlock> {
+    if !p.operator(Op::OpenCurlyBracket)? {
+        return Ok(None);
+    }
+    let statements = parse_statements(p)?;
+    if !p.operator(Op::CloseCurlyBracket)? {
+        p.warn(WarnVar::MissingClosingCurlyBracket);
+    }
+    Ok(Some(CodeBlock { statements }))
+}
+
 /// statements
 ///     = {statement}
 ///     ;
-fn parse_statements(p: &mut Parser) -> Result<Vec<Statement>, ParserError> {
+fn parse_statements(p: &mut Parser) -> Res<Vec<Statement>> {
     let mut statements = vec![];
     while let Some(statement) = parse_statement(p)? {
         statements.push(statement);
@@ -56,13 +45,11 @@ fn parse_statements(p: &mut Parser) -> Result<Vec<Statement>, ParserError> {
 ///     = expression
 ///     | EXPRESSION_END
 ///     ;
-fn parse_statement(p: &mut Parser) -> ParseResult<Statement> {
-    if let TokenType::Operator(Operator::Semicolon) = p.token()?.token_type {
-        p.pop();
-        Ok(Some(Statement::Semicolon))
+fn parse_statement(p: &mut Parser) -> OptRes<Statement> {
+    if p.operator(Op::Semicolon)? {
+        return Ok(Some(Statement::Semicolon));
     } else if let Some(expression) = parse_expression(p)? {
-        Ok(Some(Statement::Expression(expression)))
-    } else {
-        Ok(None)
+        return Ok(Some(Statement::Expression(expression)));
     }
+    Ok(None)
 }
--- src/parser/grammar/utility.rs
+++ src/parser/grammar/utility.rs
@@ -1,3 +1,5 @@
+use crate::parser::ParserError;
+
 // Re-export traits
 pub use crate::{
     parser::{ErrorHandler, ExtScannable},
@@ -7,19 +9,19 @@ pub use crate::{
 // Re-export types with short aliases
 pub use crate::parser::{
     keywords::Keyword as Kw, operators::Operator as Op, token::TokenType as Type, Parser,
-    ParserError as Erro, ParserErrorVariant as ErroVar, ParserWarningVariant as WarnVar,
+    ParserErrorVariant as ErroVar, ParserWarningVariant as WarnVar,
 };
 
 // Named types
-pub type OptRes<T> = Result<Option<T>, Erro>;
-pub type Res<T> = Result<T, Erro>;
+pub type OptRes<T> = Result<Option<T>, ParserError>;
+pub type Res<T> = Result<T, ParserError>;
 
 // Helper methods
 pub trait ParsingHelper: ExtScannable {
     /// Whether the current parser token is a specific keyword
-    fn parse_kw(&mut self, kw1: Kw) -> Res<bool> {
-        if let Type::Keyword(kw2) = self.token()?.token_type {
-            if kw1 == kw2 {
+    fn keyword(&mut self, kw: Kw) -> Res<bool> {
+        if let Type::Keyword(actual) = self.token()?.token_type {
+            if kw == actual {
                 self.pop();
                 return Ok(true);
             }
@@ -28,9 +30,9 @@ pub trait ParsingHelper: ExtScannable {
     }
 
     /// Whether the current parser token is a specific operator
-    fn parse_op(&mut self, op1: Op) -> Res<bool> {
-        if let Type::Operator(op2) = self.token()?.token_type {
-            if op1 == op2 {
+    fn operator(&mut self, op: Op) -> Res<bool> {
+        if let Type::Operator(actual) = self.token()?.token_type {
+            if op == actual {
                 self.pop();
                 return Ok(true);
             }
@@ -39,7 +41,7 @@ pub trait ParsingHelper: ExtScannable {
     }
 
     /// Whether the current parser token is an identifier
-    fn parse_id(&mut self) -> OptRes<String> {
+    fn identifier(&mut self) -> OptRes<String> {
         if let Type::Identifier(id) = self.token()?.token_type {
             self.pop();
             return Ok(Some(id));
@@ -48,7 +50,7 @@ pub trait ParsingHelper: ExtScannable {
     }
 
     /// Whether the current parser token is a string
-    fn parse_str(&mut self) -> OptRes<String> {
+    fn string(&mut self) -> OptRes<String> {
         if let Type::String(s) = self.token()?.token_type {
             self.pop();
             return Ok(Some(s));
@@ -57,7 +59,7 @@ pub trait ParsingHelper: ExtScannable {
     }
 
     /// Whether the current parser token is an integer
-    fn parse_int(&mut self) -> OptRes<i64> {
+    fn integer(&mut self) -> OptRes<i64> {
         if let Type::Int(v) = self.token()?.token_type {
             self.pop();
             return Ok(Some(v));
@@ -66,7 +68,7 @@ pub trait ParsingHelper: ExtScannable {
     }
 
     /// Whether the current parser token is a float
-    fn parse_float(&mut self) -> OptRes<f64> {
+    fn float(&mut self) -> OptRes<f64> {
         if let Type::Float(v) = self.token()?.token_type {
             self.pop();
             return Ok(Some(v));
